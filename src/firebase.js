bddd550e6c01b2b70648d814d28fe651f5c22276
Naimish Patel <patelpavan478965@gmail.com>
2024-11-29 23:28:18 +0530

    https://github.com/pavan426344/Real-Estate

---
+++ src/firebase.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyB76JR4fuWqzBVkm9WrhnZEFbQquFeZ900",
+  authDomain: "test-31664.firebaseapp.com",
+  projectId: "test-31664",
+  storageBucket: "test-31664.appspot.com",
+  messagingSenderId: "289530309920",
+  appId: "1:289530309920:web:7bb051579c404a574f3b45"
+};
+
+// Initialize Firebase 
+export const app = initializeApp(firebaseConfig);
